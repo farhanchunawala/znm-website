583be6268875d8f1b1196ba4cb3598bbba1a438b
farhan <farhanchunawala.fc@gmail.com>
2024-04-28 15:46:44 +0530

    userModel and routes

--- app/api/user/r2/route.ts
+++ app/api/user/r2/route.ts
@@ -6,7 +6,7 @@ import User from '@/models/UserModel';
 export async function GET() {
 	try {
 		await dbConnect();
-		const user = await User.find({});
+		const user = await User.findOne({});
 		return NextResponse.json(user);
 	} catch (error) {
 		console.error('Database operation failed:', error);
--- models/UserModel.ts
+++ models/UserModel.ts
@@ -1,17 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
+const UserSchema = new mongoose.Schema(
+	{
+		name: {},
 	},
-	surname: {
-		type: String,
-		required: true,
-	},
-});
+	{ timestamps: true }
+);
 
-const User = mongoose.models.User || mongoose.model('User', UserSchema);
+// const User = mongoose.models.User || mongoose.model('User', UserSchema);
+const User = mongoose.models.User || mongoose.model('User', UserSchema, 'user');
 // const User = mongoose.model('User', UserSchema, 'user');
 
 export default User;
