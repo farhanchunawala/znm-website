bd53609f91b3faeb07cdb6ac402d62e209c210e1
farhan <farhanchunawala.fc@gmail.com>
2024-05-07 00:32:31 +0530

    add mongoose sequence

--- models/FabricModel.ts
+++ models/FabricModel.ts
@@ -1,16 +1,24 @@
 import mongoose, { Schema } from 'mongoose';
+import AutoIncrement from 'mongoose-sequence';
 
 const FabricSchema = new mongoose.Schema(
 	{
-		brand: String,
-		color: String,
+		id: {
+			type: Number,
+			unique: true,
+		},
+		name: {
+			brand: String,
+		},
 	},
 	{ timestamps: true }
 );
 
+FabricSchema.plugin(AutoIncrement(mongoose), { inc_field: 'id' });
+
 // const Fabric = mongoose.models.Fabric || mongoose.model('Fabric', FabricSchema);
 const Fabric =
-	mongoose.models.Fabric || mongoose.model('Fabric', FabricSchema, 'fabric');
-// const Fabric = mongoose.model('Fabric', FabricSchema, 'fabric');
+	mongoose.models.Fabric || mongoose.model('Fabric', FabricSchema, 'fabrics');
+// const Fabric = mongoose.model('Fabric', FabricSchema, 'fabrics');
 
 export default Fabric;
--- models/UserModel.ts
+++ models/UserModel.ts
@@ -1,14 +1,21 @@
 import mongoose, { Schema } from 'mongoose';
+import AutoIncrement from 'mongoose-sequence';
 
 const UserSchema = new mongoose.Schema(
 	{
+		id: {
+			type: Number,
+			unique: true,
+		},
 		name: {},
 	},
 	{ timestamps: true }
 );
 
+UserSchema.plugin(AutoIncrement(mongoose), { inc_field: 'id' });
+
 // const User = mongoose.models.User || mongoose.model('User', UserSchema);
-const User = mongoose.models.User || mongoose.model('User', UserSchema, 'user');
-// const User = mongoose.model('User', UserSchema, 'user');
+const User = mongoose.models.User || mongoose.model('User', UserSchema, 'users');
+// const User = mongoose.model('User', UserSchema, 'users');
 
 export default User;
